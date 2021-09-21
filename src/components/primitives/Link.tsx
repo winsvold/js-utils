4a38e4e22987fad741df90f7bf037704938b1dfc
winsvold <29202094+winsvold@users.noreply.github.com>
2021-09-21 19:13:38 +0200

    fjerner avhengighet til next som egentlig ikke maker sense her

--- src/components/primitives/Link.tsx
+++ src/components/primitives/Link.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import NextLink from "next/link";
 import { AnchorHTMLAttributes } from "react";
 import React from "react";
 
@@ -17,25 +16,10 @@ const StyledA = styled.a`
   }
 `;
 
-interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
-  /*
-    Use to indicate that this is an internal link in the nextjs-app for client-side routing
-  */
-  internalLink?: boolean;
-}
+interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {}
 
 function Link(props: Props) {
-  const { internalLink, href, ...rest } = props;
-
-  if (internalLink) {
-    return (
-      <NextLink href={href || ""} passHref={true}>
-        <StyledA {...rest} />
-      </NextLink>
-    );
-  }
-
-  return <StyledA {...rest} href={href} />;
+  return <StyledA {...props} />;
 }
 
 export default Link;
